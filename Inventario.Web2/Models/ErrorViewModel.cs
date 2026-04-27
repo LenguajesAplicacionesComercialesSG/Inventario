f1f945bb604d314afe4c40449a4ab0574ddcfb03
Alonso Chavarria <alons182@hotmail.com>
2026-04-27 17:53:27 -0600

    se elimó web2

--- Inventario.Web2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Inventario.Web2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
